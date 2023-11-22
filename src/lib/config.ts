81d6752cac795f7033f9b5c1c2ebbc961492b5e8
Nguyễn Văn Linh <vanlinh14121@gmail.com>
2023-11-23 00:26:53 +0700

    update config eslint

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,5 @@
 const IP_ADDRESS = '10.0.2.2';
 export const PORT = 5053;
 export const BACKEND_URL = `http://${IP_ADDRESS}:${PORT}`;
+
+export const CHANNEL_NOTIFY = '';
--- src/screens/Statistic/index.tsx
+++ src/screens/Statistic/index.tsx
@@ -1,6 +1,8 @@
-import { Text } from 'native-base';
+import { Button, Text } from 'native-base';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { backendService } from '../../services';
 
 // type Props = HomeTabScreenProps<'home'>;
 
@@ -8,6 +10,17 @@ const Statistic = () => {
   return (
     <SafeAreaView>
       <Text>Statistic page</Text>
+      <Button
+        onPress={async () => {
+          const reslute = await backendService.post('/api/users', {
+            email: 'vanlinh14121@gmail.com',
+          });
+          if (reslute.kind === 'ok') {
+            console.log(reslute.data);
+          }
+        }}>
+        Click me
+      </Button>
     </SafeAreaView>
   );
 };
