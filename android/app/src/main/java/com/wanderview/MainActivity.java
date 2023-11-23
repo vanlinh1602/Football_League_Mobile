40b61913298363dc26e65fbbff9307dd2a696c30
Nguyễn Văn Linh <vanlinh14121@gmail.com>
2023-11-23 11:01:54 +0700

    connect to firebase

--- android/app/src/main/java/com/wanderview/MainActivity.java
+++ android/app/src/main/java/com/wanderview/MainActivity.java
@@ -1,4 +1,4 @@
-package com.wanderview;
+package com.leaguemanagement;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "WanderView";
+    return "LeagueManagement";
   }
 
   /**
