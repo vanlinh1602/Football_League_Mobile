ebf560858a77eb9bdc356d4d298b1344410b825e
Nguyễn Văn Linh <vanlinh14121@gmail.com>
2023-11-27 00:11:19 +0700

    auth user

--- src/redux/sagas/user.ts
+++ src/redux/sagas/user.ts
@@ -1,18 +1,33 @@
 import { all, put, takeEvery } from 'redux-saga/effects';
 
+import { backendService } from '../../services';
 import { actions as userActions } from '../reducers/user';
 import { SignInAction } from '../types/users';
 
 function* signIn(action: SignInAction) {
-  const user = action.payload;
-  yield put(
-    userActions.fetchUser({
-      name: user.displayName!,
-      email: user.email!,
-      uid: user.uid,
-      photoURL: user.photoURL!,
-    }),
+  const { uid, email, displayName: name, photoURL } = action.payload;
+
+  const result: WithApiResult<{ role: string }> = yield backendService.post(
+    '/api/auth',
+    {
+      uid,
+      email,
+      name,
+    },
   );
+
+  if (result.kind === 'ok') {
+    const { role } = result.data;
+    yield put(
+      userActions.fetchUser({
+        name: name!,
+        email: email!,
+        uid: uid,
+        photoURL: photoURL!,
+        role,
+      }),
+    );
+  }
 }
 
 export default function* usersSaga() {
--- src/redux/types/users.ts
+++ src/redux/types/users.ts
@@ -6,6 +6,7 @@ export type UserData = {
   email: string;
   name: string;
   photoURL?: string;
+  role?: string;
 };
 
 export type UserState = {
